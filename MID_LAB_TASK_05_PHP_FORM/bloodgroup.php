8dd4b5282b40a3e26c18508ecd7b839ad6eff74c
DmSakib360 <sakib.cse.aiub@gmail.com>
2022-03-03 02:51:27 +0600

    Lab5

---
+++ MID_LAB_TASK_05_PHP_FORM/bloodgroup.php
@@ -0,0 +1,41 @@
+<?php
+
+$bloodGroup="";
+$error="";
+if(isset($_GET['submit'])){
+	if(_GET['bloodGroup']==null){
+    	$error="Invalid blood group!";
+	}
+	else{$bloodGroup=$_GET['blood Group'];}
+}
+
+?>
+
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method="GET" action="#">
+
+	  
+	  	<fieldset>
+        <legend>Blood Group</legend>
+	  		
+        <select name="bloodgroup" >
+		<option value="A+">A+</option>
+		<option value="B+">B+</option>
+		<option value="AB+">AB+</option>
+		<option value="o+">O+</option>
+		<option value="A-">A-</option>
+		<option value="B-">B-</option>
+		<option value="AB-">AB-</option>
+		<option value="o-">O-</option></select>
+        <br><br>
+	  		
+	  	<input type="submit" value="Submit">
+	  </fieldset>
+	</form>
+</body>
+
+</html>
